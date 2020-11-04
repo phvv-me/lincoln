f9964da389fbe3c49d5c2533bad8e70006d1f450
Pedro WSL <pedrohen@petco.com>
2020-11-03 22:36:23 -0300

    bug fix in region name

--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 
-dynamodb = boto3.resource('dynamodb', region_name="us-east-2")
+dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
 table = dynamodb.Table('bot')
 
 
